12e4e7a33312aac60f90c9c216a27590c9063c2f
Scott Munro <scottmunro@utexas.edu>
2016-04-13 16:39:45 -0500

    implemented doodling around the options

--- appdirectory/HUD.cpp
+++ appdirectory/HUD.cpp
@@ -1,10 +1,43 @@
 #include "HUD.h"
+#include <algorithm>
 
 const Ogre::String HUD::windowName = "HUD";
 
 HUD::HUD(GUISystem& gui)
-    : mGUI(gui), mRoot(nullptr), mOptionSelected(0), mItemsMenuVisible(false) {
+    : mGUI(gui), mRoot(nullptr), mOptionSelected(3), mItemsMenuVisible(false) {
+
     CEGUI::WindowManager& wmgr = CEGUI::WindowManager::getSingleton();
     mRoot = wmgr.loadLayoutFromFile("menu.layout");
+
+    auto MenuFrame = mRoot->getChild("Menu_Frame");
+
+    mOptionSelects[3] = MenuFrame->getChild("Physical_select");
+    mOptionSelects[2] = MenuFrame->getChild("Special_select");
+    mOptionSelects[1] = MenuFrame->getChild("Items_select");
+    mOptionSelects[0] = MenuFrame->getChild("Guard_select");
+
     mGUI.addAndSetWindowGroup(HUD::windowName, mRoot);
+}
+
+void HUD::injectKeyDown(const OIS::KeyEvent& arg) {
+    if (!mItemsMenuVisible) {
+        int oldOption = mOptionSelected;
+        switch(arg.key) {
+            case OIS::KC_UP:
+                mOptionSelected = std::min(3, mOptionSelected + 1);
+                break;
+            case OIS::KC_DOWN:
+                mOptionSelected = std::max(0, mOptionSelected - 1);
+                break;
+            default:
+                return;
+        }
+        mOptionSelects[oldOption]->hide();
+        mOptionSelects[mOptionSelected]->show();
+        mOptionSelects[mOptionSelected]->activate();
+    }
+}
+
+void HUD::injectKeyUp(const OIS::KeyEvent& arg) {
+    // pass
 }
--- appdirectory/HUD.h
+++ appdirectory/HUD.h
@@ -4,16 +4,19 @@
 #include "GUISystem.h"
 
 #include <OgreString.h>
-
+#include <OISKeyboard.h>
 #include <CEGUI/CEGUI.h>
 
 class HUD {
 public:
     HUD(GUISystem& gui);
+    void injectKeyDown(const OIS::KeyEvent& arg);
+    void injectKeyUp(const OIS::KeyEvent& arg);
 
     GUISystem& mGUI;
     CEGUI::Window* mRoot;
     int mOptionSelected;
+    CEGUI::Window* mOptionSelects[4];
     bool mItemsMenuVisible; 
 
     static const Ogre::String windowName;

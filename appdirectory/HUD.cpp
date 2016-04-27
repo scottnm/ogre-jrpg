a0fdfa90dac5532e5cef900f38861d4fb519f310
Scott Munro <scottmunro@utexas.edu>
2016-04-27 16:21:03 -0500

    skips over items when out, still need to show disabled color

--- appdirectory/HUD.cpp
+++ appdirectory/HUD.cpp
@@ -111,6 +111,9 @@ void HUD::injectKeyDown(const OIS::KeyEvent& arg) {
             case OIS::KC_UP:
                 notifyHUDNavigation();
                 mActionOptionFocused = std::max(0, mActionOptionFocused - 1);
+                if (inventory.items.size() == 0 && mActionOptionFocused == ITEMS_ID) {
+                    mActionOptionFocused = SPECIAL_ID;
+                }
                 if (myPartyWaiting.at(myPartyFocused)->info().specialPoints == 0 &&
                         mActionOptionFocused == SPECIAL_ID) {
                     mActionOptionFocused = PHYSICAL_ID;
@@ -122,6 +125,9 @@ void HUD::injectKeyDown(const OIS::KeyEvent& arg) {
                 if (myPartyWaiting.at(myPartyFocused)->info().specialPoints == 0 &&
                         mActionOptionFocused == SPECIAL_ID) {
                     mActionOptionFocused = ITEMS_ID;
+                }
+                if (inventory.items.size() == 0 && mActionOptionFocused == ITEMS_ID) {
+                    mActionOptionFocused = GUARD_ID;
                 }
                 break;
             case OIS::KC_RETURN:

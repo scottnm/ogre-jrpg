7bc6015407db7bba068e2635176f8ba60a3916f3
Scott Munro <scottmunro@utexas.edu>
2016-05-03 19:54:57 -0500

    replaced the current particle system with a particle system queue and replaced the particle type class enum with an equivalent regular enum

--- appdirectory/ParticleController.cpp
+++ appdirectory/ParticleController.cpp
@@ -3,128 +3,94 @@
 
 int ParticleController::particleControllerId = 0;
 
-ParticleController::ParticleController(Ogre::SceneManager* scnMgr, Ogre::SceneNode* parent)
-    : currentSys(nullptr) {
+ParticleController::ParticleController(Ogre::SceneManager* scnMgr, Ogre::SceneNode* parent) {
     const Ogre::String id = Ogre::StringConverter::toString(particleControllerId++);
 
     auto guardParticles = scnMgr->createParticleSystem("Guard_P" + id, "Guard"); 
     guardParticles->setEmitting(false);
     auto guardNode = parent->createChildSceneNode("Guard_N" + id);
     guardNode->attachObject(guardParticles);
-    guardSys = guardParticles;
+    systems[PT_Guard] = guardParticles;
 
     auto physicalParticles = scnMgr->createParticleSystem("Physical_P" + id, "Physical3"); 
     physicalParticles->setEmitting(false);
     auto physicalNode = parent->createChildSceneNode("Physical_N" + id);
     physicalNode->attachObject(physicalParticles);
     physicalNode->setPosition(0, 125, -15);
-    physicalSys = physicalParticles;
+    systems[PT_Physical] = physicalParticles;
 
     auto itemParticles = scnMgr->createParticleSystem("Item_P" + id, "Item"); 
     itemParticles->setEmitting(false);
     auto itemNode = parent->createChildSceneNode("Item_N" + id);
     itemNode->attachObject(itemParticles);
     itemNode->setPosition(0, 200, 0);
-    itemSys = itemParticles;
+    systems[PT_Item] = itemParticles;
 
     auto fireParticles = scnMgr->createParticleSystem("Fire_P" + id, "Fire"); 
     fireParticles->setEmitting(false);
     auto fireNode = parent->createChildSceneNode("Fire_N" + id);
     fireNode->attachObject(fireParticles);
     fireNode->setPosition(0, 100, 0);
-    fireSys = fireParticles;
+    systems[PT_Fire] = fireParticles;
 
     auto iceParticles = scnMgr->createParticleSystem("Ice_P" + id, "Ice"); 
     iceParticles->setEmitting(false);
     auto iceNode = parent->createChildSceneNode("Ice_N" + id);
     iceNode->attachObject(iceParticles);
     iceNode->setPosition(0, 100, 0);
-    iceSys = iceParticles;
+    systems[PT_Ice] = iceParticles;
 
     auto flareParticles = scnMgr->createParticleSystem("Flare_P" + id, "Flare");
     flareParticles->setEmitting(false);
     auto flareNode = parent->createChildSceneNode("Flare_N" + id);
     flareNode->attachObject(flareParticles);
     flareNode->setPosition(0, 100, 0);
-    flareSys = flareParticles;
+    systems[PT_Flare] = flareParticles;
 }
 
 void ParticleController::stopEmittingAll(void) {
-    guardSys->setEmitting(false);
-    guardSys->setVisible(false);
-    physicalSys->setEmitting(false);
-    physicalSys->setVisible(false);
-    itemSys->setEmitting(false);
-    itemSys->setVisible(false);
-    fireSys->setEmitting(false);
-    fireSys->setVisible(false);
-    iceSys->setEmitting(false);
-    iceSys->setVisible(false);
-    flareSys->setEmitting(false);
-    flareSys->setVisible(false);
+    for (int i = 0; i < 6; ++i) {
+        auto ps = systems[i];
+        ps->setEmitting(false);
+        ps->setVisible(false);
+    }
 }
 
 void ParticleController::runParticleSystem(ParticleType pt, ParticleEndCheckCallback endCheck,
         ParticleCallback onEnd) {  
-    if (currentSys != nullptr) {
-        this->onEnd(); 
-        currentSys->setEmitting(false);
-        currentSys->setVisible(false);
-        currentSys = nullptr;
-    }
-
-    this->endCheck = endCheck;
-    this->onEnd = onEnd;
-    switch(pt) {
-        case ParticleType::Guard:
-            currentSys = guardSys;
-            break;
-        case ParticleType::Item:
-            currentSys = itemSys;
-            break;
-        case ParticleType::Physical:
-            currentSys = physicalSys;
-            break;
-        case ParticleType::Fire:
-            currentSys = fireSys;
-            break;
-        case ParticleType::Ice:
-            currentSys = iceSys;
-            break;
-        case ParticleType::Flare:
-            currentSys = flareSys;
-            break;
-        default:
-            break;
-    }
-    currentSys->setEmitting(true);
-    currentSys->setVisible(true);
+    Ogre::ParticleSystem* system = systems[pt];
+    system->setEmitting(true);
+    system->setVisible(true);
+    mEventQueue.push_back(ParticleEvent(system, endCheck, onEnd));
 }
 
 void ParticleController::updateParticles(void) {
-    if (currentSys == nullptr) {
-        return;
-    }
-
-    if (endCheck()) {
-        onEnd();
-        currentSys->setEmitting(false);
-        currentSys->setVisible(false);
-        currentSys = nullptr;
+    auto i = mEventQueue.begin();
+    while (i != mEventQueue.end()) {
+        ParticleEvent& evt = *i;
+        if (evt.endCheck()) {
+            evt.onEnd();
+            evt.system->setEmitting(false);
+            evt.system->setVisible(false);
+            i = mEventQueue.erase(i);
+        }
+        else {
+            ++i;
+        }
     }
 }
 
 
 bool ParticleController::checkFireCollision(Ogre::SceneNode* target) {
-    return _checkCollision(fireSys, target);
+    return _checkCollision(systems[PT_Fire], target);
 }
 
 bool ParticleController::checkIceCollision(Ogre::SceneNode* target) {
-    return _checkCollision(iceSys, target);
+    return _checkCollision(systems[PT_Ice], target);
 }
 
 bool ParticleController::checkFlareCollision(Ogre::SceneNode* target) {
-    return _checkCollision(flareSys, target);
+    return _checkCollision(systems[PT_Flare], target);
 }
 
 bool ParticleController::_checkCollision(Ogre::ParticleSystem* psys, Ogre::SceneNode* target) {
